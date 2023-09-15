3600487f5c633ab2ad5cf628ccbaf3f492e6a625
wayapay <abraham@wayapay.com>
2023-09-15 12:44:52 +0300

    commented error show

--- cpanel/library/daloradius.conf.php
+++ cpanel/library/daloradius.conf.php
@@ -1,9 +1,9 @@
 <?php
 
 // show all errors
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
 
 // read from .env file 
 require_once __DIR__ . '/read_env_file.php';
