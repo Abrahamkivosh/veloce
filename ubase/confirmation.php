9dbf5f42738b6c882b7c29a93e70568b7d529ca9
Abrahamkivosh <abrahamkivosh@gmail.com>
2025-02-13 23:39:18 +0300

    added

--- ubase/confirmation.php
+++ ubase/confirmation.php
@@ -1,183 +1,149 @@
 <?php
 
-header("Content-Type:application/json");
-// include smsService
+header("Content-Type: application/json");
+
+// Include required files
 require_once "../services/smsService.php";
-
-$smsService = new smsService();
-
-// inclue database file in shared folder
 require_once "../shared/database.php";
 
-if (!isset($_GET["token"])) {
-    echo "Technical error";
+// Initialize SMS service
+$smsService = new smsService();
+
+// Authorization token check
+$authToken = "217ab0d4de6d426df670759321392fc2";
+if (!isset($_GET["token"]) || $_GET["token"] !== $authToken) {
+    echo json_encode(["error" => "Unauthorized access"]);
     exit();
 }
 
-if ($_GET["token"] != '217ab0d4de6d426df670759321392fc2') {
-    echo "Invalid authorization";
+// Get the input JSON data
+$request = file_get_contents("php://input");
+if (!$request) {
+    echo json_encode(["error" => "Invalid input"]);
     exit();
 }
 
-if (!$request = file_get_contents('php://input')) {
-    echo "Invalid input";
+// Decode JSON into an array
+$data = json_decode($request, true);
+if (!is_array($data)) {
+    echo json_encode(["error" => "Invalid JSON format"]);
     exit();
 }
 
-//Put the json string that we received from Safaricom to an array
-$array = json_decode($request, true);
-$transactiontype = mysqli_real_escape_string($con, $array['TransactionType']);
-$transid = mysqli_real_escape_string($con, $array['TransID']);
-$transtime = mysqli_real_escape_string($con, $array['TransTime']);
-$transamount = mysqli_real_escape_string($con, $array['TransAmount']);
-$businessshortcode = mysqli_real_escape_string($con, $array['BusinessShortCode']);
-$billrefno = mysqli_real_escape_string($con, $array['BillRefNumber']);
-$invoiceno = mysqli_real_escape_string($con, $array['InvoiceNumber']);
-$msisdn = mysqli_real_escape_string($con, $array['MSISDN']);
-$orgaccountbalance = mysqli_real_escape_string($con, $array['OrgAccountBalance']);
-$firstname = mysqli_real_escape_string($con, $array['FirstName']);
-$middlename = mysqli_real_escape_string($con, $array['MiddleName']);
-$lastname = mysqli_real_escape_string($con, $array['LastName']);
+// Extract and sanitize input data
+$fields = [
+    'TransactionType',
+    'TransID',
+    'TransTime',
+    'TransAmount',
+    'BusinessShortCode',
+    'BillRefNumber',
+    'InvoiceNumber',
+    'MSISDN',
+    'OrgAccountBalance',
+    'FirstName',
+    'MiddleName',
+    'LastName'
+];
 
-$sql = "INSERT INTO mpesa_payments
-(
-TransactionType,
-TransID,
-TransTime,
-TransAmount,
-BusinessShortCode,
-BillRefNumber,
-InvoiceNumber,
-MSISDN,
-FirstName,
-MiddleName,
-LastName,
-OrgAccountBalance
-)
-VALUES
-(
-'$transactiontype',
-'$transid',
-'$transtime',
-'$transamount',
-'$businessshortcode',
-'$billrefno',
-'$invoiceno',
-'$msisdn',
-'$firstname',
-'$middlename',
-'$lastname',
-'$orgaccountbalance'
-)";
-
-$sqlis = "SELECT account,username,firstname,lastname FROM userinfo WHERE account = '$billrefno'";
-$resulte = mysqli_query($con, $sqlis);
-$fullname = "";
-$username = "";
-// return $resulte;
-while ($row = mysqli_fetch_array($resulte, MYSQLI_ASSOC)) {
-    # code...
-    $username = ($row['username']);
-    $fullname = ($row['firstname'] . " " . $row['lastname']);
+foreach ($fields as $field) {
+    $$field = isset($data[$field]) ? mysqli_real_escape_string($con, $data[$field]) : '';
 }
 
-$phone = "";
+// Insert transaction data into the database
+$sql = "INSERT INTO mpesa_payments (TransactionType, TransID, TransTime, TransAmount, BusinessShortCode, BillRefNumber, InvoiceNumber, MSISDN, FirstName, MiddleName, LastName, OrgAccountBalance) 
+        VALUES ('$TransactionType', '$TransID', '$TransTime', '$TransAmount', '$BusinessShortCode', '$BillRefNumber', '$InvoiceNumber', '$MSISDN', '$FirstName', '$MiddleName', '$LastName', '$OrgAccountBalance')";
+mysqli_query($con, $sql);
 
-$sq = "SELECT * FROM userbillinfo WHERE username = '$username'";
+// Fetch user details based on BillRefNumber
+$userQuery = "SELECT account, username, firstname, lastname FROM userinfo WHERE account = '$BillRefNumber'";
+$userResult = mysqli_query($con, $userQuery);
+$user = mysqli_fetch_assoc($userResult);
 
-$result = mysqli_query($con, $sq);
-while ($row = mysqli_fetch_array($result)) {
-    $plan = ($row['planName']);
-    $account = ($row['account']);
-    $phone = ($row['phone']);
+if (!$user) {
+    echo json_encode(["error" => "User not found"]);
+    exit();
+}
+
+$username = $user['username'];
+$fullname = "{$user['firstname']} {$user['lastname']}";
+
+// Fetch user billing info
+$billingQuery = "SELECT planName, account, phone, balance FROM userbillinfo WHERE username = '$username'";
+$billingResult = mysqli_query($con, $billingQuery);
+$billingInfo = mysqli_fetch_assoc($billingResult);
+
+if (!$billingInfo) {
+    echo json_encode(["error" => "Billing information not found"]);
+    exit();
 }
 
 
-$sqli2 = "SELECT * FROM billing_plans WHERE planName = '$plan'";
-$result4 = mysqli_query($con, $sqli2);
-while ($row = mysqli_fetch_array($result4)) {
-    $plancost = ($row['planCost']);
-    $timebank = ($row['planTimeBank']);
-}
-$sqli = "SELECT * FROM userbillinfo WHERE username = '$username'";
-$result = mysqli_query($con, $sqli);
-while ($row = mysqli_fetch_array($result)) {
-    # code...
-    if (($row['balance']) > 0) {
-        $plan = ($row['planName']);
-        $account = ($row['account']);
-        $balance = ($row['balance']);
-    } else {
 
-        $account = ($row['account']);
-        $balance = ($row['balance']);
-        $balance = 0;
-    }
+$plan = $billingInfo['planName'];
+$phone = $billingInfo['phone'];
+$account = $billingInfo['account'];
+$balance = $billingInfo['balance'] ?? 0;
+
+// Fetch plan cost and time bank
+$planQuery = "SELECT planCost, planTimeBank FROM billing_plans WHERE planName = '$plan'";
+$planResult = mysqli_query($con, $planQuery);
+$planInfo = mysqli_fetch_assoc($planResult);
+
+if (!$planInfo) {
+    echo json_encode(["error" => "Plan details not found"]);
+    exit();
 }
 
-$sqli3 = "SELECT * FROM radcheck WHERE ((username = '$username')&&(attribute = 'Expiration'))";
-$result3 = mysqli_query($con, $sqli3);
-while ($row = mysqli_fetch_array($result3)) {
-    # code...
-    $rexp = ($row['value']);
-    $eXpiry = strtotime($row['value']);
-    $timenow = time();
+$planCost = $planInfo['planCost'];
+$timeBank = $planInfo['planTimeBank'];
 
-    if ($timenow > $eXpiry) {
-        $timenew = (($eXpiry + ($timenow - $eXpiry)) + ($timebank));
-        $newdate = date("d M Y H:i:s", $timenew);
-        //echo $newdate."</br>";
+// Fetch expiration date
+$expirationQuery = "SELECT value FROM radcheck WHERE username = '$username' AND attribute = 'Expiration'";
+$expirationResult = mysqli_query($con, $expirationQuery);
+$expirationData = mysqli_fetch_assoc($expirationResult);
 
-    } else {
-        $timenew = $eXpiry + $timebank;
-        $newdate = date("d M Y H:i:s", $timenew);
-        //echo $newdate."</br>";
-    }
-}
-
-if ($transamount > $plancost) {
-
-    $bal = ($transamount - $plancost);
-
-    $new_balance = ($bal + $balance);
-
-    $sqli4 = "UPDATE radcheck SET value='$newdate' WHERE ((attribute='Expiration')&&(username='$username'))";
-    $result10 = mysqli_query($con, $sqli4);
-    $sqli5 = "UPDATE userbillinfo SET balance='$new_balance' WHERE username='$username'";
-    $result11 = mysqli_query($con, $sqli5);
-
-    $response = $smsService->confirmationOfPayment($phone, $newdate, $new_balance);
-} else if ($transamount < $plancost) {
-
-    $new_amount = ($transamount + $balance);
-
-    if ($new_amount >= $plancost) {
-
-        $new_balance = $new_amount - $plancost;
-
-        $sqli7 = "UPDATE radcheck SET value='$newdate' WHERE ((attribute='Expiration')&&(username='$username'))";
-        $result12 = mysqli_query($con, $sqli7);
-        $sqli8 = "UPDATE userbillinfo SET balance='$new_balance' WHERE username='$username'";
-        $result13 = mysqli_query($con, $sqli8);
-        $smsService->confirmationOfPayment($phone, $newdate, $new_balance);
-    } else {
-        $diff = $plancost - $new_amount;
-        $sqli9 = "UPDATE userbillinfo SET balance='$new_amount' WHERE username='$username'";
-        $result14 = mysqli_query($con, $sqli9);
-
-        $smsService->lessBalanceNotice($phone, $fullname, $account, $new_amount, $diff);
-    }
+if ($expirationData) {
+    $expiryTimestamp = strtotime($expirationData['value']);
+    $currentTimestamp = time();
+    $newTimestamp = max($currentTimestamp, $expiryTimestamp) + $timeBank;
+    $newExpiryDate = date("d M Y H:i:s", $newTimestamp);
 } else {
-    $sqli11 = "UPDATE radcheck SET value='$newdate' WHERE ((attribute='Expiration')&&(username='$username'))";
-    $result16 = mysqli_query($con, $sqli11);
-
-    $smsService->confirmationOfPayment($phone, $newdate, $balance);
+    echo json_encode(["error" => "Expiration data not found"]);
+    exit();
 }
 
-if (!mysqli_query($con, $sql)) {
-    echo mysqli_error($con);
+// Process payment and update user balance
+if ($TransAmount >= $planCost) {
+    $newBalance = ($TransAmount - $planCost) + $balance;
+
+    mysqli_query($con, "UPDATE radcheck SET value='$newExpiryDate' WHERE username='$username' AND attribute='Expiration'");
+    mysqli_query($con, "UPDATE userbillinfo SET balance='$newBalance' WHERE username='$username'");
+
+    $smsService->confirmationOfPayment($phone, $newExpiryDate, $newBalance);
 } else {
-    echo '{"ResultCode":0,"ResultDesc":"Confirmation received successfully"}';
+    $totalAmount = $TransAmount + $balance;
+
+
+
+    if ($totalAmount >= $planCost) {
+        $newBalance = $totalAmount - $planCost;
+
+        mysqli_query($con, "UPDATE radcheck SET value='$newExpiryDate' WHERE username='$username' AND attribute='Expiration'");
+        mysqli_query($con, "UPDATE userbillinfo SET balance='$newBalance' WHERE username='$username'");
+
+        $smsService->confirmationOfPayment($phone, $newExpiryDate, $newBalance);
+    } else {
+        $remainingAmount = $planCost - $totalAmount;
+
+        mysqli_query($con, "UPDATE userbillinfo SET balance='$totalAmount' WHERE username='$username'");
+
+        $smsService->lessBalanceNotice($phone, $fullname, $account, $totalAmount, $remainingAmount);
+    }
 }
 
+// Return success response
+echo json_encode(["ResultCode" => 0, "ResultDesc" => "Confirmation received successfully"]);
+
+// Close the database connection
 mysqli_close($con);
