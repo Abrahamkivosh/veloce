474bee95b9d5136b1e1a98c561694be7793c1723
Abrahamkivosh <abrahamkivosh@gmail.com>
2025-04-30 21:25:50 +0300

    Refactor smsService class and remove commented-out transaction logging in confirmation.php

--- services/smsService.php
+++ services/smsService.php
@@ -3,57 +3,59 @@
 require_once  '../cpanel/library/read_env_file.php';
 
 
-class smsService {
+class smsService
+{
     private  $fnum, $text, $text_username, $text_password, $text_issn;
-    public function __construct() {
+    public function __construct()
+    {
         $this->text_username = getenv('TEXTUSERNAME');
         $this->text_password = getenv('TEXTPASSWORD');
         $this->text_issn = getenv('TEXT_ISSN');
-
     }
 
-    private function sendSms() {
+    private function sendSms()
+    {
         $url = "https://client.airtouch.co.ke:9012/sms/api/?issn=$this->text_issn&msisdn=$this->fnum&text=$this->text&username=$this->text_username&password=$this->text_password";
-       
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, $url);
         $output = curl_exec($ch);
-        if ($output===FALSE) {
-            echo "cURL Error:". curl_error($ch);
+        if ($output === FALSE) {
+            echo "cURL Error:" . curl_error($ch);
             // Log error to file
-            $error = "cURL Error:". curl_error($ch);
+            $error = "cURL Error:" . curl_error($ch);
             $error_file = "../error.txt";
-            $file = fopen ($error_file, "a");
+            $file = fopen($error_file, "a");
             fwrite($file, $error);
             fclose($file);
             return $error;
-        }else{
+        } else {
             return $output;
         }
         curl_close($ch);
     }
 
-    public function setPhone($fnum) {
-     
+    public function setPhone($fnum)
+    {
+
         // check if number start with 254
         $firstchar = $fnum[0];
         if ($firstchar == "0") {
             $newnum = substr($fnum, 1);
             $fnum = "254" . $newnum;
-        } else {
-            $fnum = $fnum;
         }
         // remove spaces
         $fnum = str_replace(" ", "", $fnum);
         // remove + sign
         $fnum = str_replace("+", "", $fnum);
 
-        
+
         $this->fnum = $fnum;
     }
-    public function setText($text) {
+    public function setText($text)
+    {
         // replace spaces with plus sign
         // $text = str_replace(" ", "+", $text);
         //  add url encoding
@@ -63,12 +65,12 @@ class smsService {
 
     public function welcomeSMS($phone, $fullName, $accountNumber, $package, $userName, $password)
     {
-        $message = "Dear $fullName, Welcome to Christa Networks Limited. Your account number is $accountNumber. Your plan is $package. Please use Mpesa Paybill No. 4043919 to complete your payment.To access your online portal use https://www.christanetworks.co.ke/selfcare Username - $userName Password - $password";   
+        $message = "Dear $fullName, Welcome to Christa Networks Limited. Your account number is $accountNumber. Your plan is $package. Please use Mpesa Paybill No. 4043919 to complete your payment.To access your online portal use https://www.christanetworks.co.ke/selfcare Username - $userName Password - $password";
         $this->setPhone($phone);
         $this->setText($message);
         $this->sendSms();
     }
-    public function renewalReminder ($phone, $fullName, $accountNumber,$package, $expiryDate)
+    public function renewalReminder($phone, $fullName, $accountNumber, $package, $expiryDate)
     {
         $message = "Dear $fullName Your account $accountNumber will expire on $expiryDate. Please topup to avoid disconnection. Your plan is $package. Mpesa paybill: 4043919. Thank you. Christa Networks. To access your online portal use https://www.christanetworks.co.ke/selfcare";
         $this->setPhone($phone);
@@ -81,7 +83,7 @@ class smsService {
         $message = "Your subscription has been renewed to $newExpiryDate. Your Wallet balance is Ksh.$balance. To access your online portal use  https://www.christanetworks.co.ke/selfcare";
         $this->setPhone($phone);
         $this->setText($message);
-      
+
         return $this->sendSms();
     }
 
@@ -102,7 +104,7 @@ class smsService {
         $message = "Dear $fullName, Your account $accountNumber is less by KES $lessAmount to renew your subscription. Your balance is KES $balance.";
         $this->setPhone($phone);
         $this->setText($message);
-      
+
         return $this->sendSms();
     }
 
@@ -112,7 +114,6 @@ class smsService {
         $this->setText($message);
         return $this->sendSms();
     }
-
 }
 
 // $smsService = new smsService();
